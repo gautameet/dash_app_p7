6daf59b9902c4739257936e0e476a71a64efff7b
gautameet <145467217+gautameet@users.noreply.github.com>
2024-02-02 19:16:18 +0100

    Update dashappp7.py

--- dashappp7.py
+++ dashappp7.py
@@ -10,3 +10,18 @@ import pickle
 import sys
 
 st.title("Test project 7 dashboaard")
+
+feat = ['SK_ID_CURR','TARGET','DAYS_BIRTH','NAME_FAMILY_STATUS','CNT_CHILDREN',
+        'DAYS_EMPLOYED','NAME_INCOME_TYPE','AMT_INCOME_TOTAL','AMT_CREDIT','AMT_ANNUITY']
+
+# Nombre de ligne
+num_rows = 100000
+
+# Original Data
+zip_file_train = ZipFile('./sample_application_train.zip')
+print(zip_file_train.namelist())
+try:
+    raw_train = pd.read_csv(zip_file_train.open('sample_application_train.csv'), usecols=feat, nrows=num_rows)
+#with ZipFile(zip_file_train, 'r') as zip_train:
+except Exception as e:
+    print(f'Error:{e}')
