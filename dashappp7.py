0067d2bb727f488a33998d6dffdd1e05075d8d1f
gautameet <145467217+gautameet@users.noreply.github.com>
2024-02-02 19:14:16 +0100

    Update dashappp7.py

--- dashappp7.py
+++ dashappp7.py
@@ -1,8 +1,9 @@
  ## Import des librairies
 import streamlit as st
+import altair as alt              # for data visualtization
 from PIL import Image
 import requests
-
+import matplotlib.pyplot as plt
 
 
 st.title("Test project 7 dashboaard")
