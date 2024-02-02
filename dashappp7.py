88a7e47531a3af545f34defca9a4e58ca9791cfd
gautameet <145467217+gautameet@users.noreply.github.com>
2024-02-02 19:14:51 +0100

    Update dashappp7.py

--- dashappp7.py
+++ dashappp7.py
@@ -3,7 +3,8 @@ import streamlit as st
 import altair as alt              # for data visualtization
 from PIL import Image
 import requests
-import matplotlib.pyplot as plt
+import os
+os.environ["SKLEARN_ALLOW_DEPRECATED_SKLEARN_PACKAGE_INSTALL"] = "True"
 
 
 st.title("Test project 7 dashboaard")
